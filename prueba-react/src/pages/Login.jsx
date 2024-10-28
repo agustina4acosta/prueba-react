d3b58bb8f5545e19675248093df6e54a5455dde5
agustina4acosta <agustinaderek09@gmail.com>
2024-10-27 23:43:53 -0300

    set up react-google-login

--- prueba-react/src/pages/Login.jsx
+++ prueba-react/src/pages/Login.jsx
@@ -1,7 +1,33 @@
 import React from 'react'
+import { GoogleLogin } from 'react-google-login';
+import { useNavigate } from 'react-router-dom';
 const Login = () => {
+  const navigate = useNavigate();
+ const onSuccess = (response) =>{
+  console.log('Login success:', response)
+  navigate('/')
+ }
+
+ const onFailure = (response) =>{
+  console.log('Login Failed:', response)
+ }
+
   return (
-    <div>Login</div>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-white">
+      <h1 className='text-4xl font-bold text-orange-500 mb-6'>
+        Iniciar sesión
+      </h1>
+      
+      <GoogleLogin
+      clientId='191542565581-q5gol85oa85o9r2stod8aq3q648m075g.apps.googleusercontent.com'
+      buttonText='Sign in'
+      onSuccess={onSuccess}
+      onFailure={onFailure}
+      cookiePolicy={'single_host_origin'}
+      />
+      
+
+    </div>
   )
 }
 export default Login
